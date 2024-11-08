8fc794283e7c774962eed6b0d71b03315f07ceab
Jacob Maizel <jacobmaizel@MacBook-Pro-de-Jacob.local>
2024-11-08 01:24:10 +0100

    solong

--- parcing.c
+++ parcing.c
@@ -3,14 +3,54 @@
 /*                                                        :::      ::::::::   */
 /*   parcing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jmaizel <jmaizel@student.s19.be>           +#+  +:+       +#+        */
+/*   By: jacobmaizel <jacobmaizel@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 17:04:42 by jmaizel           #+#    #+#             */
-/*   Updated: 2024/11/07 17:05:26 by jmaizel          ###   ########.fr       */
+/*   Updated: 2024/11/08 01:24:01 by jacobmaizel      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int is_rectangular(char **map)
+#include "game.h"
+
+int is_map_rectangular(char **map)
 {
+    int i;
+    int line_length;
+
+    if (!map || !map[0])
+    {
+        ft_printf("Erreur : carte vide ou non initialisée\n");
+        return (0);
+    }
+    line_length = ft_strlen(map[0]);
+    i = 0;
+    while (map[i])
+    {
+        if (ft_strlen(map[i]) != line_length)
+        {
+            ft_printf("Erreur : la carte n'est pas rectangulaire\n");
+            return (0);
+        }
+        i++;
+    }
+    return (1);
+}
+
+int are_map_border_valide(char **map)
+{
+	int i;
+	int start;
+	int end;
+
+	start = 0;
+	i = 0;
+	end = ft_strlen(map[i]);
+	while (map[i])
+	{
+		if (map[start][0] != '1')
+		{
+			ft_printf("")
+		}
+	}
 
 }
